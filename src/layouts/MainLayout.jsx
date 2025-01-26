015d5a8cfa88325b8a315eb08e069dc4e8043d77
Phoenix-Dev <116443234+Phoenix-Dev1@users.noreply.github.com>
2025-01-26 14:17:00 +0200

    [Add] - Vercel Web Analytics

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollToTop from "../components/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 const MainLayout = () => {
   return (
@@ -9,6 +10,7 @@ const MainLayout = () => {
       <Navbar />
       <main className="flex-grow">
         <Outlet />
+        <Analytics />
       </main>
       <Footer />
       <ScrollToTop />
